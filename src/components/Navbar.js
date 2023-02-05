e476cac65db10f8069ff33646ca89faea40b0985
Evans Obi <vnsobi@gmail.com>
2023-02-05 19:15:41 +0100

    fork link update

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -94,7 +94,7 @@ function NavBar() {
 
             <Nav.Item className="fork-btn">
               <Button
-                href="https://github.com/VnsObi/Portfolio"
+                href="https://github.com/VnsObi/My_Portfolio"
                 target="_blank"
                 className="fork-btn-inner"
               >
